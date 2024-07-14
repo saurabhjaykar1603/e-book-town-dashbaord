bb51e9ef2dfddd26ff1ba7b9d39a3db66d88f62e
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-07-14 23:24:25 +0530

    feat : add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Button } from "./components/ui/button"
 
 
 function App() {
 
   return (
     <>
-      <div>Welcome to E Book Town </div>
+      <div>Welcome to E Book Town  <Button variant={"destructive"} className="mt-12">ShadCn</Button></div>
     </>
   )
 }
