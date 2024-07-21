a24bf1dc0772f54d30faf6136667925b498346d7
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-07-21 21:02:59 +0530

    "Removed unused icons and links from DashboardLayout, updated remaining links and text"

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -3,17 +3,13 @@ import {
   Book,
   CircleUser,
   Home,
-  LineChart,
   Menu,
   Package,
   Package2,
   Search,
-  ShoppingCart,
-  Users,
 } from "lucide-react";
 import { Link, Navigate, NavLink, Outlet } from "react-router-dom";
 import { Button } from "../components/ui/button";
-import { Badge } from "../components/ui/badge";
 import {
   Card,
   CardContent,
@@ -122,45 +118,22 @@ function DashboardLayout() {
                   className="flex items-center gap-2 text-lg font-semibold"
                 >
                   <Package2 className="h-6 w-6" />
-                  <span className="sr-only">Acme Inc</span>
+                  <span className="sr-oly">E Book Town</span>
                 </Link>
                 <Link
-                  to="#"
+                  to="/dashboard/home"
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                 >
                   <Home className="h-5 w-5" />
-                  Dashboard
+                  Home
                 </Link>
+
                 <Link
-                  to="#"
-                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
-                >
-                  <ShoppingCart className="h-5 w-5" />
-                  Orders
-                  <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                    6
-                  </Badge>
-                </Link>
-                <Link
-                  to="#"
+                  to="/dashboard/books"
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                 >
                   <Package className="h-5 w-5" />
-                  Products
-                </Link>
-                <Link
-                  to="#"
-                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                >
-                  <Users className="h-5 w-5" />
-                  Customers
-                </Link>
-                <Link
-                  to="#"
-                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                >
-                  <LineChart className="h-5 w-5" />
-                  Analytics
+                  Books
                 </Link>
               </nav>
               <div className="mt-auto">
